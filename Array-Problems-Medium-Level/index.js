f346d166e77f7512fa7b57a82fa26b171a1622ff
bhushan-biradar-75 <bhushanbiradar75@gmail.com>
2023-10-21 09:21:49 +0530

    Accumulated Product Array Problem Solved With Multiplication Logic

--- Array-Problems-Medium-Level/index.js
+++ Array-Problems-Medium-Level/index.js
@@ -8,15 +8,49 @@
 // accumulatingArray([]) ➞ []
 
 const accumulatingArray = (arrayItem) => {
-    if (arrayItem) {
-        for (let i = 0; i < arrayItem.length - 1; i++) {
-            indexSum = arrayItem[i] + arrayItem[i + 1] //calculating current & next index sum & stores in indexSum
-            arrayItem[i + 1] = indexSum;
-        }
-    } 
-}
-accumulatingItem = [1, 2, 3, 4] //Passing array by reference
-// accumulatingItem = [1, 2, 3, 6, 5, 12, 7, 20] //Passing array by reference
-accumulatingArray(accumulatingItem)
+  if (arrayItem) {
+    for (let i = 0; i < arrayItem.length - 1; i++) {
+      indexSum = arrayItem[i] + arrayItem[i + 1]; //calculating current & next index sum & stores in indexSum
+      arrayItem[i + 1] = indexSum;
+    }
+  }
+  return arrayItem;
+};
+let accumulatingItem = [1, 2, 3, 4]; //Passing array by reference
+let accumulatingArrayResult = accumulatingArray(accumulatingItem);
 //[OUTPUT]// [1,3,6,10]
-console.log("acc",accumulatingItem)
+console.log("accumulatingArray", accumulatingArrayResult);
+
+// ===========================================================================================================================
+
+// accumulatingProduct([1, 2, 3, 4, 5]) ➞ [1, 2, 6, 24, 120]
+// // [1, 2, 6, 24] can be written as [1, 1 x 2, 1 x 2 x 3, 1 x 2 x 3 x 4]
+
+// accumulatingProduct([1, 5, 7]) ➞ [1, 5, 35]
+
+// accumulatingProduct([1, 0, 1, 0]) ➞ [1, 0, 0, 0]
+
+// accumulatingProduct([]) ➞ []
+
+const accumulatingProduct = (arrayItem) => {
+  if (arrayItem) {
+    for (let i = 0; i < arrayItem.length - 1; i++) {
+      let currentIndex = arrayItem[i];
+      let nextIndex = arrayItem[i + 1];
+      let indexSum;
+      if (i === 0) {
+        //calculating current & next index to stores in indexSum
+        indexSum = currentIndex * nextIndex;
+      } else if (i > 0) {
+        indexSum = currentIndex * nextIndex;
+      }
+      //puting updated index value in array
+      arrayItem[i + 1] = indexSum;
+    }
+  }
+  return arrayItem;
+};
+let accumulatingProductItem = [1, 2, 3, 4, 5]; //Passing array by reference
+let accumulatingProductResult = accumulatingProduct(accumulatingProductItem);
+console.log("accumulatingProduct", accumulatingProductResult);
+//[OUTPUT]//➞ [1, 2, 6, 24, 120]
