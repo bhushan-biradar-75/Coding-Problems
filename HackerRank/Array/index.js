7a71c534afeb3144359b2b86f56edda2f45c0f9c
bhushan-biradar-75 <bhushanbiradar75@gmail.com>
2023-10-29 23:02:32 +0530

    HackerRank 2D Array Problem Solved

--- HackerRank/Array/index.js
+++ HackerRank/Array/index.js
@@ -27,30 +27,60 @@
 // Input - [1 4 3 2]
 // Output - [2 3 4 1]
 
-// function reverseArray(a) {
-//     let newArray = [];
-//     for(let i = a.length - 1; i >= 0; i--)
-//     {
-//         newArray[newArray.length] = a[i];
-//     }
-//     return newArray;
-// }
-// var item = [1,4,3,2]
-// var reverseArray = reverseArray(item)
-// console.log("reverseArray",reverseArray)
+function reverseArray(a) {
+    let newArray = [];
+    for(let i = a.length - 1; i >= 0; i--)
+    {
+        newArray[newArray.length] = a[i];
+    }
+    return newArray;
+}
+var item = [1,4,3,2]
+var reverseArray = reverseArray(item)
+console.log("reverseArray",reverseArray)
+
+// Given a  2D Array, :
+
+// 1 1 1 0 0 0
+// 0 1 0 0 0 0
+// 1 1 1 0 0 0
+// 0 0 0 0 0 0
+// 0 0 0 0 0 0
+// 0 0 0 0 0 0
+// An hourglass in  is a subset of values with indices falling in this pattern in 's graphical representation:
+
+// a b c
+//   d
+// e f g
+// There are  hourglasses in . An hourglass sum is the sum of an hourglass' values. Calculate the hourglass sum for every hourglass in , then print the maximum hourglass sum. The array will always be .
+
+// Sample Input
+
+// 1 1 1 0 0 0
+// 0 1 0 0 0 0
+// 1 1 1 0 0 0
+// 0 0 2 4 4 0
+// 0 0 0 2 0 0
+// 0 0 1 2 4 0
+
+// Sample Output
+
+// 19
 
 function getArray(item) {
-    // console.log(item.length)
-    let maxSum = -63;
+    let maxSum = -63
     for(let i = 0; i < 4; i++)
     {
         for(let j = 0; j < 4; j++)
         {
 
             let top = item[i][j] + item[i][j+1] + item[i][j+2]; 
-            console.log("---->",top)
+            let mid = item[i][j+1]
+            let bot = item[i][j+1] + item[i][j+2] + item[i][j+3]; 
+            let totalSum = top+mid+bot
+            let maxSum = Math.max(totalSum);
+            return maxSum;
         }
-        // break;
     }
 }
 
@@ -60,3 +90,4 @@ let TwoDArray = [
     [1, 1, 1, 0, 0, 0],
 ]
 let findHourSum = getArray(TwoDArray) 
+console.log(findHourSum)
